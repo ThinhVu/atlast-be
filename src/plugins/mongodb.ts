781b0110056769097031bdde426714a702a26fae
ThinhVu <thinhvux@protonmail.com>
2024-03-07 01:14:12 +0700

    update mongo client connection

--- src/plugins/mongodb.ts
+++ src/plugins/mongodb.ts
@@ -8,23 +8,37 @@ export function getColl<TSchema = any>(name: string) : Collection<TSchema> {
 
 async function connect() {
    try {
-      const {
-        DATABASE_HOST,
-        DATABASE_USERNAME,
-        DATABASE_PASSWORD,
-        DATABASE_NAME,
-      } = process.env;
+      const {DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME} = process.env;
+      const config = {
+        username: DATABASE_USERNAME,
+        password: DATABASE_PASSWORD,
+        dbName: DATABASE_NAME
+      }
       console.log(`[mongodb] Connecting to database ${DATABASE_NAME}`)
-      const url = DATABASE_USERNAME
-        ? `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}`
-        : `mongodb://${DATABASE_HOST}`;
-      client = new MongoClient(url);
+      client = connectMongoClient(config);
       db = client.db(DATABASE_NAME);
       console.log('[mongodb] Connected to server!')
    } catch (error) {
       console.error('[mongodb] Failed to connect. Reason:', error)
       process.exit(1)
    }
+}
+
+export type ConnectClientConfig = {
+  username?: string,
+  password?: string,
+  dbName: string,
+}
+
+export function connectMongoClient(config: ConnectClientConfig) {
+  const dbHost = process.env.DATABASE_HOST
+  if (!dbHost) throw new Error("Missing DATABASE_HOST env variable")
+  const {username, password, dbName} = config;
+  const url = username
+    ? `mongodb://${username}:${password}@${dbHost}?authSource=${dbName}`
+    : `mongodb://${dbHost}?authSource=${dbName}`;
+  client = new MongoClient(url);
+  return client;
 }
 
 export function getDb(name: string) {
