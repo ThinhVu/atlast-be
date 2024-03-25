ab222c2a03b8e5583d4625701d8ed05ad4d649f5
ThinhVu <thinhvux@protonmail.com>
2024-03-25 11:36:13 +0700

    update db webhook

--- src/db/models/db-webhook.ts
+++ src/db/models/db-webhook.ts
@@ -3,13 +3,9 @@ import {Indexed} from "../../utils/types";
 
 export type IDbWebhook = Partial<{
     _id: ObjectId;
-    userId: Indexed<ObjectId>;
-    dbName: string;
-    name: string;
-    //database contain collections that they want to watch the changes
-    colName: string;
-    //collection that they want to watch the changes
+    dbName: Indexed<string>;
+    colName: Indexed<string>; //collection that they want to watch the changes
+    desc: string;
     to: string; //link to user api;
-    //if they don't choose, that mean they want all CRUD
     createDt: Date; //the day user register the webhook service
 }>
--- src/mongodb-change-stream/watch-change.ts
+++ src/mongodb-change-stream/watch-change.ts
@@ -1,62 +1,63 @@
-import {getColl, getDb} from "../plugins/mongodb";
+import axios from 'axios';
+import _ from 'lodash';
+import {getDb} from "../plugins/mongodb";
 import {Model} from "../db/models";
-const {execAxios} = require('./utils');
-const axios = require('axios');
-import {Db} from 'mongodb';
+import {ObjectId} from "mongodb";
 
-let db: Db
-let changeStreams = []
-const cacheChangeStream: Map<any, any> = new Map();
+const changeStreamCache: Map<ObjectId, ChangeStreamCacheValue> = new Map();
+
+type ChangeStreamCacheValue = {
+  webhookURL: string,
+  watcher: any, // TODO
+}
 
 export async function watchCollection() {
-
-    try {
-        let changeStreamChange = false;
-
-        // async function startChangeStream(change) {
-        //     if (changeStreamChange) {
-        //         changeStreams.forEach(changeStream => {
-        //             if (!cacheChangeStream[changeStream]) {
-        //                 cacheChangeStream[changeStream] = change.fullDocument/* change stream collection */
-        //             }
-        //         }
-        //     }
-        //     await setupChangeStreams()
-        // }
-
-        async function setupChangeStreams() {
-            const changeStreams = await Model.DbWebhook.find({}).toArray();
-            if (!changeStreams || changeStreams.length === 0) return;
-
-            changeStreams.forEach(changeStream => {
-                if (!cacheChangeStream.has(changeStream._id)) {
-                    cacheChangeStream.set(changeStream._id, getDb(changeStream.dbName).collection(changeStream.colName).watch());
-                    cacheChangeStream.get(changeStream._id).on('change', (change) => {
-                        const userApi = changeStream.to;
-                        return execAxios(axios.post(`${userApi}`, change.fullDocument))
-                    });
-                }
-                //     if (!cacheChangeStream[changeStream._id]) {
-                //         cacheChangeStream[changeStream._id] = getDb(changeStream.dbName).collection(changeStream.colName).watch();
-                //         cacheChangeStream[changeStream].on('change', (change) => {
-                //             const userApi = changeStream.to;
-                //             return execAxios(axios.post(`${userApi}`, change.fullDocument))
-                //         });
-                //     }
-                // },
-            })
-        }
-        await setupChangeStreams();
-
-        const originalChangeStream = await Model.DbWebhook.watch();
-        originalChangeStream.on('change', (change) => {
-            console.log('Change detected in original collection', change);
-            changeStreamChange = true
-            // await startChangeStream(change)
-        });
-    } catch (e) {
-    throw new Error("Error happened", e)
+  try {
+    async function setupChangeStreams() {
+      const changeStreams = await Model.DbWebhook.find().toArray();
+      if (_.isEmpty(changeStreams)) return;
+      for (const changeStream of changeStreams) {
+        const watcher = getDb(changeStream.dbName).collection(changeStream.colName).watch();
+        watcher.on('change', (change) => axios.post(`${changeStream.to}`, change.fullDocument));
+        changeStreamCache.set(changeStream._id, watcher);
+      }
     }
+
+    const originalChangeStream = await Model.DbWebhook.watch();
+    originalChangeStream.on('change', (change) => {
+      console.log('Change detected in original collection', change);
+      // db nao? collection nao?
+      // find watcher -> stop  1 - 1 (dbName_collName)
+      // xoa watcher di
+      // xem operator la gi: insert, delete, update
+      const operator = ''
+      const doc = change.fullDocument;
+      switch (operator) {
+        case 'insert':
+          if (changeStreamCache.get(doc._id)) {
+            // ??
+          }
+          break;
+        case 'update':
+          if (changeStreamCache.get(doc._id)) {
+            // TODO: assign nhung thong tin can thiet
+            changeStreamCache.get(doc._id).webhookURL = doc.webhookURL
+          }
+          break;
+        case 'delete':
+          if (changeStreamCache.get(doc._id)) {
+            changeStreamCache.get(doc._id).watcher.close()
+            changeStreamCache.delete(doc._id)
+          }
+          break;
+      }
+    });
+
+
+    await setupChangeStreams();
+  } catch (e) {
+    throw new Error("Error happened", e)
+  }
 }
 
 
