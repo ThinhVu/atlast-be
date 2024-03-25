bdb065d475e53137f2897280069bd25bad502849
ThinhVu <thinhvux@protonmail.com>
2024-03-25 10:59:29 +0700

    chore

--- src/logic/explore.ts
+++ src/logic/explore.ts
@@ -8,9 +8,9 @@ export async function getDbCollection(userId: ObjectId, dbId: ObjectId) {
     return db.listCollections().toArray()
 }
 
-export async function createNewCollection(userId: ObjectId, dbId: ObjectId, colName:string) {
+export async function createNewCollection(userId: ObjectId, dbId: ObjectId, colName: string) {
     const {dbName} = await Model.Database.findOne({_id: dbId}, {projection: {dbName: 1}});
     const db = getDb(dbName)
-    const test= await db.collection(colName).insertOne({name:"haha"})
+    await db.collection(colName).insertOne({deleted: true})
     await db.collection(colName).deleteOne()
 }
