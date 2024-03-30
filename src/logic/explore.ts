3bd96e5135bc6c0dd3315c0f27b11e0515688f87
lananhvu2001 <vulananh2001@gmail.com>
2024-03-30 11:11:41 +0700

    change in explore.ts

--- src/logic/explore.ts
+++ src/logic/explore.ts
@@ -2,20 +2,20 @@ import {ObjectId} from "mongodb";
 import {Model} from "../db/models";
 import {getDb} from "../plugins/mongodb";
 
-export async function getDbCollection(userId: ObjectId, dbId: ObjectId) {
+export async function getDbCollection(dbId: ObjectId) {
     const {dbName} = await Model.Database.findOne({_id: dbId}, {projection: {dbName: 1}});
     const db = getDb(dbName)
     return db.listCollections().toArray()
 }
 
-export async function createNewCollection(userId: ObjectId, dbId: ObjectId, colName: string) {
+export async function createNewCollection(dbId: ObjectId, colName: string) {
     const {dbName} = await Model.Database.findOne({_id: dbId}, {projection: {dbName: 1}});
     const db = getDb(dbName)
     await db.collection(colName).insertOne({deleted: true})
     await db.collection(colName).deleteOne()
 }
 
-export async function deleteCollection(userId: ObjectId, dbId: ObjectId, colName: string) {
+export async function deleteCollection(dbId: ObjectId, colName: string) {
     const {dbName} = await Model.Database.findOne({_id: dbId}, {projection: {dbName: 1}});
     const db = getDb(dbName)
     const isWebhookExist = await Model.DbWebhook.find({dbName: dbName, colName: colName})
--- src/routes/explore.route.ts
+++ src/routes/explore.route.ts
@@ -1,7 +1,7 @@
 import {Request, Router} from "hyper-express";
 import {requireUser, UserProps} from "../middlewares/auth";
 import $ from "../utils/safe-call";
-import {getDbCollection, createNewCollection} from "../logic/explore";
+import {getDbCollection, createNewCollection, deleteCollection} from "../logic/explore";
 import DataParser from "../utils/data-parser";
 
 
@@ -11,14 +11,22 @@ export default async function useExplore(parentRouter: Router) {
     router.get('/:id',
         {middlewares: [requireUser]},
         $(async (req: Request<UserProps>) => {
-            return getDbCollection(req.locals.user._id, DataParser.objectId(req.path_parameters.id))
+            return getDbCollection(DataParser.objectId(req.path_parameters.id))
         }));
 
     router.post('/:id',
         {middlewares: [requireUser]},
         $(async (req: Request<UserProps>) => {
             const {colName} = await req.json();
-            return createNewCollection(req.locals.user._id, DataParser.objectId(req.path_parameters.id), colName)
+            return createNewCollection(DataParser.objectId(req.path_parameters.id), colName)
         }));
+
+    router.delete('/:id',
+        {middlewares: [requireUser]},
+        $(async (req: Request<UserProps>) => {
+            const {colName} = await req.json();
+            return deleteCollection(DataParser.objectId(req.path_parameters.id), colName)
+        }));
+
     parentRouter.use('/explore', router);
 }
