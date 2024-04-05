f5491f91d70109d793f47ecd594acbd18e4360bb
lananhvu2001 <vulananh2001@gmail.com>
2024-04-05 09:07:51 +0700

    merge with new commit

--- src/logic/user-collection.ts
+++ src/logic/user-collection.ts
@@ -1,10 +1,7 @@
-import _ from 'lodash';
 import {ObjectId} from "mongodb";
 import {Model} from '../db/models';
-// import uuid from 'time-uuid';
 import {connectMongoClient} from "../plugins/mongodb";
 import {Db, MongoClient} from 'mongodb';
-import DataParser from "../utils/data-parser";
 
 let db: Db
 let client: MongoClient
@@ -12,23 +9,8 @@ let cache = new Set()
 
 export async function connectDb(userId: ObjectId, dbId: ObjectId) {
   const {username, password, dbName} = await Model.Database.findOne({_id: dbId, userId: userId});
-  try {
-    const dbHost = process.env.DATABASE_HOST
-    if (!dbHost) {
-      throw new Error("Missing DATABASE_HOST env variable")
-    }
-    const dbHostContainsConfig = dbHost.indexOf('?')
-    const url = username
-      ? `mongodb://${username}:${password}@${dbHost}${dbHostContainsConfig ? '&' : '?'}authSource=${dbName}`
-      : `mongodb://${dbHost}?authSource=${dbName}`;
-    client = new MongoClient(url);
-    db = client.db(dbName);
-    console.log(`successful accessing to mongo client!`)
-    return db
-  } catch (e) {
-    console.error('[mongodb] Failed to connect. Reason:', e)
-    process.exit(1)
-  }
+  const mongoClient = connectMongoClient({username, password, dbName, authSource: dbName})
+  return mongoClient.db(dbName);
 }
 
 
