24090687b70cddeac33a9a0a519de288aa20fc7a
lananhvu2001 <vulananh2001@gmail.com>
2024-04-05 08:24:54 +0700

    change user-collection

--- src/logic/user-collection.ts
+++ src/logic/user-collection.ts
@@ -2,65 +2,106 @@ import _ from 'lodash';
 import {ObjectId} from "mongodb";
 import {Model} from '../db/models';
 // import uuid from 'time-uuid';
-import {connectMongoClient, getDb} from "../plugins/mongodb";
-import {Db} from 'mongodb';
+import {connectMongoClient} from "../plugins/mongodb";
+import {Db, MongoClient} from 'mongodb';
+import DataParser from "../utils/data-parser";
 
 let db: Db
+let client: MongoClient
 let cache = new Set()
-let documents = []
 
 
-export async function connectDb(userId: ObjectId, dbId: ObjectId) {
-  const {username, password, dbName} = await Model.Database.findOne({_id: dbId, userId: userId})
-  const mongoClient = connectMongoClient({username, password, dbName})
-  db = mongoClient.db(dbName);
-  return db
-}
+// export async function connectDb(userId: ObjectId, dbId: ObjectId) {
+//   const {username, password, dbName} = await Model.Database.findOne({userId: userId, dbId: dbId});
+//   try {
+//     const mongoClient = connectMongoClient({username, password, dbName})
+//     db = mongoClient.db(dbName);
+//     console.log(`successful accessing to mongo client!`)
+//     return db
+//   } catch (e) {
+//     console.error('[mongodb] Failed to connect. Reason:', e)
+//     process.exit(1)
+//   }
+// }
 
-export async function createNewDoc(userId: ObjectId, dbId: ObjectId, colName: string, doc) {
-  await connectDb(userId, dbId);
-  const collection = db.collection(colName);
-  await collection.insertOne(doc)
-  await getFieldNames(doc)
-}
-
-export async function deleteDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId) {
-  await connectDb(userId, dbId);
-  const collection = db.collection(colName);
-  await collection.deleteOne({_id: docId})
-}
-
-export async function updateDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId, doc) {
-  await connectDb(userId, dbId);
-  const collection = db.collection(colName);
-  await collection.updateOne({_id: docId}, {$set:doc})
-  await getFieldNames(doc)
-}
-
-export async function getFieldNames(doc) {
-  const fields = Object.keys(doc);
-  fields.forEach(field => {
-    if (!cache.has(field)) {
-      cache.add(field);
+export async function connectDb(dbId: ObjectId) {
+  const {username, password, dbName} = await Model.Database.findOne({_id: dbId});
+  try {
+    const dbHost = process.env.DATABASE_HOST
+    if (!dbHost) {
+      throw new Error("Missing DATABASE_HOST env variable")
     }
-  });
-  return fields
+    const dbHostContainsConfig = dbHost.indexOf('?')
+    const url = username
+      ? `mongodb://${username}:${password}@${dbHost}${dbHostContainsConfig ? '&' : '?'}authSource=${dbName}`
+      : `mongodb://${dbHost}?authSource=${dbName}`;
+    client = new MongoClient(url);
+    db = client.db(dbName);
+    console.log(`successful accessing to mongo client!`)
+    return db
+  } catch (e) {
+    console.error('[mongodb] Failed to connect. Reason:', e)
+    process.exit(1)
+  }
 }
 
-export async function getDocs(userId: ObjectId, dbId: ObjectId, colName: string, page: number) {
-  await connectDb(userId, dbId);
+// export async function createNewDoc(userId: ObjectId, dbId: ObjectId, colName: string, doc) {
+//   await connectDb(userId, dbId);
+//   const collection = db.collection(colName);
+//   await collection.insertOne(doc)
+//   return getFieldNames(userId, dbId, colName, doc)
+// }
+
+
+export async function createNewDoc(dbId: ObjectId, colName: string, doc) {
+  await connectDb(dbId);
+  const document = JSON.parse(doc)
+  const collection = db.collection(colName);
+  const {insertedId} = await collection.insertOne(document)
+  document._id = insertedId
+  return document;
+  //return getFieldNames(userId, dbId, colName, doc)
+}
+
+// export async function deleteDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId) {
+//   await connectDb(userId, dbId);
+//   const collection = db.collection(colName);
+//   await collection.deleteOne({_id: docId})
+// }
+
+// export async function updateDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId, doc) {
+//   await connectDb(userId, dbId);
+//   const collection = db.collection(colName);
+//   await collection.updateOne({_id: docId}, {$set:doc})
+//   return getFieldNames(userId, dbId, colName, doc)
+// }
+//
+// export async function getFieldNames(userId: ObjectId, dbId: ObjectId, colName: string, doc) {
+//   if (cache.size === 0 ) {
+//     await connectDb(userId, dbId);
+//     const document = db.collection(colName).findOne({})
+//     if (!document) return null
+//     Object.keys(document).forEach(field => {
+//       if (!cache.has(field)) {
+//         cache.add(field);
+//       }
+//     });
+//   } else {
+//     Object.keys(doc).forEach(field => {
+//       if (!cache.has(field)) {
+//         cache.add(field);
+//       }
+//     });
+//   }
+//   return cache
+// }
+//
+export async function getDocs(dbId: ObjectId, colName: string, page: number) {
+  await connectDb(dbId)
   const collection = db.collection(colName);
   const pageSize = 10;
-  async function fetchPage(page) {
-    const skip = (page - 1) * pageSize;
-    documents = await collection.find().skip(skip).limit(pageSize).toArray();
-    // documents = await collection.find().toArray()
-    console.log(`docs: ${JSON.stringify(documents)}`)
-    console.log(`Content of page ${page}:`, documents);
-    if (documents.length === pageSize) {
-      await fetchPage(page + 1);
-    }
-  }
-  await fetchPage(page);
+  const skip = (page - 1) * pageSize;
+  const documents = await collection.find().skip(skip).limit(pageSize).toArray();
+  if (!documents) return
   return documents
 }
--- src/routes/user-collection.route.ts
+++ src/routes/user-collection.route.ts
@@ -1,7 +1,7 @@
 import {Request, Router} from "hyper-express";
 import {requireUser, UserProps} from "../middlewares/auth";
 import $ from "../utils/safe-call";
-import {createNewDoc, deleteDoc, updateDoc, getDocs} from "../logic/user-collection";
+import {createNewDoc, deleteDoc, updateDoc, getDocs, connectDb} from "../logic/user-collection";
 import DataParser from "../utils/data-parser";
 
 
@@ -10,45 +10,44 @@ export default async function useUserCol(parentRouter: Router) {
   const router = new Router();
 
   router.get('/:dbId/:col/:page',
-    {middlewares: [requireUser]},
+    // {middlewares: [requireUser]},
     $(async (req: Request<UserProps>) => {
-      const userId = req.locals.user._id
       const dbId = DataParser.objectId(req.path_parameters.dbId)
       const colName = DataParser.str(req.path_parameters.col)
       const page = DataParser.number(req.path_parameters.page,1)
-      return getDocs(userId, dbId, colName, page)
+      return getDocs(dbId, colName, page)
     }));
 
   router.post('/:dbId/:col',
-    {middlewares: [requireUser]},
+    // {middlewares: [requireUser]},
     $(async (req: Request<UserProps>) => {
-      const doc = req.json()
-      const userId = req.locals.user._id
+      const doc = await req.json();
+      // const userId = req.locals.user._id
       const dbId = DataParser.objectId(req.path_parameters.dbId)
       const colName = DataParser.str(req.path_parameters.col)
-      return createNewDoc(userId, dbId, colName, doc)
+      return createNewDoc(dbId, colName, doc)
     }));
 
-  router.post('/dbId/:col/:id',
-    {middlewares: [requireUser]},
-    $(async (req: Request<UserProps>) => {
-      const doc = req.json()
-      const userId = req.locals.user._id
-      const dbId = DataParser.objectId(req.path_parameters.dbId)
-      const colName = DataParser.str(req.path_parameters.col)
-      const id = DataParser.objectId(req.path_parameters.id)
-      return updateDoc(userId, dbId, colName, id, doc)
-    }));
-
-  router.delete('/dbId/:col/:id',
-    {middlewares: [requireUser]},
-    $(async (req: Request<UserProps>) => {
-      const userId = req.locals.user._id
-      const dbId = DataParser.objectId(req.path_parameters.dbId)
-      const colName = DataParser.str(req.path_parameters.col)
-      const id = DataParser.objectId(req.path_parameters.id)
-      return deleteDoc(userId, dbId, colName, id)
-    }));
+  // router.post('/dbId/:col/:id',
+  //   {middlewares: [requireUser]},
+  //   $(async (req: Request<UserProps>) => {
+  //     const doc = req.json()
+  //     const userId = req.locals.user._id
+  //     const dbId = DataParser.objectId(req.path_parameters.dbId)
+  //     const colName = DataParser.str(req.path_parameters.col)
+  //     const id = DataParser.objectId(req.path_parameters.id)
+  //     return updateDoc(userId, dbId, colName, id, doc)
+  //   }));
+  //
+  // router.delete('/dbId/:col/:id',
+  //   {middlewares: [requireUser]},
+  //   $(async (req: Request<UserProps>) => {
+  //     const userId = req.locals.user._id
+  //     const dbId = DataParser.objectId(req.path_parameters.dbId)
+  //     const colName = DataParser.str(req.path_parameters.col)
+  //     const id = DataParser.objectId(req.path_parameters.id)
+  //     return deleteDoc(userId, dbId, colName, id)
+  //   }));
 
   parentRouter.use('/user-collection', router);
 }
