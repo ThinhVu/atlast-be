49460782fd5d6a74251c5ea6f277cbc08f2cc9d1
ThinhVu <thinhvux@protonmail.com>
2024-04-09 01:56:54 +0700

    improve perfm

--- src/logic/user-collection.ts
+++ src/logic/user-collection.ts
@@ -1,30 +1,39 @@
 import {ObjectId} from "mongodb";
 import {Model} from '../db/models';
 import {connectMongoClient} from "../plugins/mongodb";
-import {Db, MongoClient} from 'mongodb';
+import {Db} from 'mongodb';
+import dayjs from 'dayjs';
+
+const dbCache = new Map<string, {db: Db, expireAt: Date}>();
 
 export async function connectDb(userId: ObjectId, dbId: ObjectId) {
+  const cacheKey = `${userId.toString()}_${dbId.toString()}`
+  const cacheValue = dbCache.get(cacheKey);
+  if (cacheValue && dayjs(cacheValue.expireAt).isAfter(dayjs()))
+    return cacheValue.db
   const {username, password, dbName} = await Model.Database.findOne({_id: dbId, userId: userId});
   const mongoClient = connectMongoClient({username, password, dbName, authSource: dbName})
-  return mongoClient.db(dbName);
+  const db = mongoClient.db(dbName)
+  dbCache.set(cacheKey, { db, expireAt: dayjs().add(30, 'minutes').toDate()})
+  return db;
 }
 
-export async function createDoc(userId, dbId: ObjectId, colName: string, doc: any) {
+export async function createDoc(userId: ObjectId, dbId: ObjectId, colName: string, doc: any) {
   const db = await connectDb(userId, dbId);
   const collection = db.collection(colName);
-  return await collection.insertOne(doc)
+  return collection.insertOne(doc);
 }
 
 export async function updateDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId, doc) {
   const db = await connectDb(userId, dbId);
   const collection = db.collection(colName);
-  return await collection.updateOne({_id: docId}, {$set:doc})
+  return collection.updateOne({_id: docId}, {$set:doc});
 }
 
 export async function deleteDoc(userId: ObjectId, dbId: ObjectId, colName: string, docId: ObjectId) {
   const db = await connectDb(userId, dbId);
   const collection = db.collection(colName);
-  await collection.deleteOne({_id: docId})
+  return collection.deleteOne({_id: docId})
 }
 
 export async function getDocs(userId: ObjectId ,dbId: ObjectId, colName: string, page: number) {
